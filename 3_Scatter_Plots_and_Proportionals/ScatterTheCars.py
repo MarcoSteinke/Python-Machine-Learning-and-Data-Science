28c2d12d01fc96ccf19ca6435fdc411f11a99bb0
maste150hhu <mastremolten2@gmail.com>
2019-12-13 12:50:58 +0100

    add example ScatterTheCars.py and Cars.png

--- 3_Scatter_Plots_and_Proportionals/ScatterTheCars.py
+++ 3_Scatter_Plots_and_Proportionals/ScatterTheCars.py
@@ -2,6 +2,13 @@ import numpy
 import matplotlib.pyplot as plt
 
 
-# 
-agesOfCars = [5,7,8,7,2,17,2,9,4,11,12,9,6]
-speedOfCars = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+# As mentioned in the description, we got two 
+# data sets which consist of the ages and the speeds
+# of the cars
+agesOfCars = [6,9,12,11,4,9,2,17,2,7,8,7,5]
+speedOfCars = [286, 285, 277, 278, 294, 287, 303, 286, 311, 288, 287, 286, 299]
+
+# In the next step we have to scatter the cars with the
+# method seen in ScatterPlot.py or 3.1
+plt.scatter(agesOfCars, speedOfCars)
+plt.show();
