614addcf850442d6a0835be1f12c07f2335cda10
maste150hhu <mastremolten2@gmail.com>
2019-12-13 11:14:47 +0100

    update GeneratingBigData.py

--- Data Distribution/GeneratingBigData.py
+++ Data Distribution/GeneratingBigData.py
@@ -9,13 +9,12 @@ from scipy import stats
 # In this example I am generating 200 values between 40 and 68 
 bigData = numpy.random.uniform(40, 68, 200)
 
-for num in bigData:
-    num = round(num)
-
 print(bigData)
 print
 # Now lets do the same as before:
 
-print("Mean = " + str(numpy.mean(bigData)))
-print("Median = " + str(numpy.median(bigData)))
-print("Mode = " + str(stats.mode(bigData)))
+print("Mean               = " + str(numpy.mean(bigData)))
+print("Median             = " + str(numpy.median(bigData)))
+print("Standard Deviation = " + str(numpy.std(bigData)))
+print("Variance           = " + str(numpy.var(bigData)))
+print("Mode               = " + str(stats.mode(bigData)))
